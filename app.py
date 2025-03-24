3b497dbfece66d4a4602c0bd17d6dac84f39f0be
dthatprince <nwaekwuprince@gmai.com>
2025-03-25 00:23:39 +0100

    add status column to track task completion

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ class Todo(db.Model):
     id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
     title: Mapped[str] = mapped_column(db.String(250), nullable=False)
     desc: Mapped[str] = mapped_column(db.Text, nullable=False)
+    status = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return {"id": self.id, "title": self.title, "desc": self.desc, "status": self.status}
 
 
 
