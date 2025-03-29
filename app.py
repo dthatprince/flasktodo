97c77247106ed0d2e125ce474e72f371b943a61b
dthatprince <nwaekwuprince@gmai.com>
2025-03-29 23:44:08 +0100

    updated data models for user and tasks

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify
 import db
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Boolean, Integer, String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+
+from datetime import datetime
+from sqlalchemy import Boolean, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from flask_migrate import Migrate
 
 from flask_restful import Resource, Api
@@ -25,15 +27,44 @@ db.init_app(app)
 # Initialize Flask-Migrate 
 migrate = Migrate(app, db)
 
-class Todo(db.Model):
-    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(db.String(250), nullable=False)
-    desc: Mapped[str] = mapped_column(db.Text, nullable=False)
-    status: Mapped[bool] = mapped_column(Boolean, default=False)
+# Data Model for Users
+class User(db.Model):
+    __tablename__ = "users"
 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    
+    tasks = relationship("Task", back_populates="user", cascade="all, delete-orphan")
+    
     def to_dict(self):
-        return {"id": self.id, "title": self.title, "desc": self.desc, "status": self.status}
+        return {"id": self.id, "username": self.username, "email": self.email}
+    
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
+# Data Model for Tasks
+class Todo(db.Model):
+    __tablename__ = "tasks"
 
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(db.String(250), nullable=False)
+    description: Mapped[str] = mapped_column(db.Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.utcnow)
+    due_date: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    status: Mapped[bool] = mapped_column(Boolean, default=False)
+    
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    user = relationship("User", back_populates="tasks")
+    
+    def to_dict(self):
+        return {"id": self.id, "title": self.title, "desc": self.description, 
+                "description": self.description, "created_at": self.created_at,
+                 "due_date": self.due_date, "user.id": self.user_id, "status": self.status}
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
 
 
 
