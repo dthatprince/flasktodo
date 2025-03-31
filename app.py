e04733a1322559c20e0b0089df75d7c97d299fc5
faith <faith@marascientific.org>
2025-03-31 22:16:40 +0300

    Load configurations from the config file

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask
 from db import db
 from flask_migrate import Migrate
 from todo.todo_blueprint import todo_blueprint
+from config import DevelopmentConfig
 
 app = Flask(__name__)
 
 app.register_blueprint(todo_blueprint)
 
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///to-do-app.db" #TODO: create a configuration file to add all configurations
+# Load configurations from the config file
+app.config.from_object(DevelopmentConfig())
+
 # initialize the app with the extension
 db.init_app(app)
 
