3111c6767e999fe3a4a663b603eab3d3bb75a024
dthatprince <nwaekwuprince@gmai.com>
2025-04-05 01:01:22 +0200

    updating data models for users and tasks

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,27 @@
 from db import db
-from sqlalchemy import Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+from sqlalchemy import Boolean, Integer, Text, String, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+
+# Data Model for Tasks
 class Todo(db.Model):
-    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(db.String(250), nullable=False)
-    desc: Mapped[str] = mapped_column(db.Text, nullable=False)
-    status: Mapped[bool] = mapped_column(Boolean, default=False)
+    __tablename__ = "tasks"
 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=datetime.utcnow)
+    due_date: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    status: Mapped[bool] = mapped_column(Boolean, default=False)
+    
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    user = relationship("User", back_populates="tasks")
+    
     def to_dict(self):
-        return {"id": self.id, "title": self.title, "desc": self.desc, "status": self.status}
+        return {"id": self.id, "title": self.title, "desc": self.description, 
+                "description": self.description, "created_at": self.created_at,
+                 "due_date": self.due_date, "user.id": self.user_id, "status": self.status}
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
